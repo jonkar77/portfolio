b05a19b271607f18a6d154cdf9b5eabe55d7cf4c
OJ <onkarjoshi296@gmail.com>
2024-03-08 19:56:35 +0530

    modified changes

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,32 +1,29 @@
-// import { ProjectCard } from "../sub/projectCard.tsx";
-// import { PROJECTS } from "@/constants";
-
 import { ProjectCard } from "../sub/projectCard";
 
 export const Projects = () => {
-    const PROJECTS = [
-        {
-          title: "Melon-Estate",
-          description:
-            'Embark on a journey through my professional evolution with the "Modern Next.js Portfolio" - a dynamic showcase of my skills, experiences, and passion for web development. Crafted with precision and powered by Next.js',
-          image: "/assets/p1.png",
-          link: "https://melon-estate.onrender.com",
-        },
-        {
-          title: "Interactive Cards Portfolio",
-          description:
-            'Step into the extraordinary world of my professional journey through the "Interactive Cards Portfolio" - an innovative and visually captivating platform that redefines the traditional portfolio experience.',
-          image: "/assets/project.png",
-          link: "https://example.com",
-        },
-        {
-            title: "Interactive Cards Portfolio",
-            description:
-              'Step into the extraordinary world of my professional journey through the "Interactive Cards Portfolio" - an innovative and visually captivating platform that redefines the traditional portfolio experience.',
-            image: "/assets/project.png",
-            link: "https://example.com",
-          },
-      ] as const;
+  const PROJECTS = [
+    {
+      title: "Melon-Estate",
+      description:
+        'Embark on a journey through my professional evolution with the "Modern Next.js Portfolio" - a dynamic showcase of my skills, experiences, and passion for web development. Crafted with precision and powered by Next.js',
+      image: "/assets/p1.png",
+      link: "https://melon-estate.onrender.com",
+    },
+    {
+      title: "Interactive Cards Portfolio",
+      description:
+        'Step into the extraordinary world of my professional journey through the "Interactive Cards Portfolio" - an innovative and visually captivating platform that redefines the traditional portfolio experience.',
+      image: "/assets/project.png",
+      link: "https://example.com",
+    },
+    {
+      title: "Another Project",
+      description:
+      'Step into the extraordinary world of my professional journey through the "Interactive Cards Portfolio" - an innovative and visually captivating platform that redefines the traditional portfolio experience.',
+      image: "/assets/project.png",
+      link: "https://example.com",
+    },
+  ];
   return (
     <section
       id="projects"
@@ -36,16 +33,16 @@ export const Projects = () => {
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-5 px-5">
-        {PROJECTS.map((project) => (
-          <div className="border border-black">
+        {PROJECTS.map((project, index) => (
+          <div key={index} className="border border-black">
             <ProjectCard
-            key={project.title}
-            src={project.image}
-            title={project.title}
-            description={project.description}
-            link={project.link}
-          />
-            </div>
+              key={project.title}
+              src={project.image}
+              title={project.title}
+              description={project.description}
+              link={project.link}
+            />
+          </div>
         ))}
       </div>
     </section>
