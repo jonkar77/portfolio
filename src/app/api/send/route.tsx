4168a75d37c583f628dc43697658d02acf145727
OJ <onkarjoshi296@gmail.com>
2024-03-11 16:11:56 +0530

    Update function signature in send route

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,16 +1,23 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
+import { Resend, CreateEmailOptions } from "resend";
+import { ReactElement } from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
+interface EmailData {
+  email: string;
+  subject: string;
+  message: string;
+}
+
+export async function POST(req: any, res: any) {
+  const { email, subject, message }: EmailData = await req.json();
   console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail,email],
+      from: fromEmail as string, // Ensure fromEmail is of type string
+      to: [fromEmail as string, email],
       subject: subject,
       react: (
         <>
@@ -20,7 +27,7 @@ export async function POST(req, res) {
           <p>{message}</p>
         </>
       ),
-    });
+    } as CreateEmailOptions); // Specify the type of the options object
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error });
